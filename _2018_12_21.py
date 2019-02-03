51411813d38abfc20e5e455ad57f0618a7864443
Walczi123 <38495867+Walczi123@users.noreply.github.com>
2019-02-03 01:01:09 +0100

    Mouse click work !!!

--- _2018_12_21.py
+++ _2018_12_21.py
@@ -1,22 +1,21 @@
-# 16_many_snakes.py
-import random
-#from collections import deque
-
+from random import randrange
 import pygame, sys
 from pygame.locals import *
+from math import ceil,floor,frexp
 
-#GAME_CELL_SIZE_PX = 20  # HAVE TO BE EVEN NUMBER
-#assert (int(GAME_CELL_SIZE_PX/2)*2) == GAME_CELL_SIZE_PX
+# # # TO CHANGE # # #
+__FPS__ = 60  # Frames Per second
+__AMOUNT_OF_BOMBS__=(16,9) # Numbers of bombs at row, at column
+__BUTTON_SIZE__=(40,40) #Size of button without perimeter
 
-#GAME_CELLS_X = 40
-#GAME_CELLS_Y = 30
+# # # NOT TO CHANGE # # #
+__BORDER__=((0.25*__BUTTON_SIZE__[0])/2,(0.25*__BUTTON_SIZE__[1])/2)  #Perimeter of button
+__PERIMETER__=((0.1*__BUTTON_SIZE__[0])//2,(0.1*__BUTTON_SIZE__[1])//2) #Perimeter of window
+__WINDOW_SIZE__=(int(__AMOUNT_OF_BOMBS__[0]*__BUTTON_SIZE__[0]+__PERIMETER__[0]*4),int(__AMOUNT_OF_BOMBS__[1]*__BUTTON_SIZE__[1]+__PERIMETER__[1]*4)) #Size of widows
 
-FPS = 30  # Frames Per second
-#MPS = 10 # Moves per second
 
-#FRAMES_PER_MOVE = FPS // MPS
-
-#random.seed(a=1)
+def mantissa(x):
+    return x-floor(x)
 
 class Sqr:
     """
@@ -40,171 +39,18 @@ class Sqr:
         self.bomb=bomb   
         self.flag=flag
         self.uncovered=uncovered
-#należy zabespieczyć
-def random_bombs(board): 
-    XSize=len(board[0])
-    YSize=len(board) 
-    buttons=XSize*YSize
-    n=int(random.randrange(10,25)*(buttons/100))
+
+def random_bombs(board=None):  
+    if board==None: return
+    buttons=__AMOUNT_OF_BOMBS__[0]*__AMOUNT_OF_BOMBS__[1]
+    n=int(randrange(10,25)*(buttons/100))
     for i in range(0,n):
-        y=random.randrange(0,YSize)
-        x=random.randrange(0,XSize)
+        y=randrange(0,__AMOUNT_OF_BOMBS__[0])
+        x=randrange(0,__AMOUNT_OF_BOMBS__[1])
         if board[y][x].bomb == False:
             board[y][x].bomb = True
         else:
             i=i-1
-
-def draw_segment(surface, color, x, y):
-    """
-    :param surface: surface to draw on
-    :param x: x coord in game cells
-    :param y: y coord in game cells
-    :return: 
-    """
-    # WHITE = (255, 255, 255)
-    position = (
-            x * GAME_CELL_SIZE_PX,
-            y * GAME_CELL_SIZE_PX,
-            GAME_CELL_SIZE_PX,
-            GAME_CELL_SIZE_PX
-    )
-    pygame.draw.rect(surface, color, position)
-
-
-def draw_food(surface, x, y):
-    """
-    :param surface: surface to draw on
-    :param x: x coord in game cells
-    :param y: y coord in game cells
-    :return: 
-    """
-    RED = (255, 0, 0)
-    position = (
-        x * GAME_CELL_SIZE_PX + GAME_CELL_SIZE_PX//2,
-        y * GAME_CELL_SIZE_PX + GAME_CELL_SIZE_PX//2)
-    pygame.draw.circle(surface, RED, position, GAME_CELL_SIZE_PX//2)
-
-
-class Snake:
-    vectors = {
-        'UP': (0, -1),
-        'DOWN': (0, 1),
-        'LEFT': (-1, 0),
-        'RIGHT': (1, 0),
-    }
-    opposites =  {
-        'UP': 'DOWN',
-        'DOWN': 'UP',
-        'LEFT': 'RIGHT',
-        'RIGHT': 'LEFT',
-    }
-
-    def __init__(
-        self,
-        food,
-        color=(255, 255, 255),
-        key_mapping=None,
-        initial_segments=None,
-    ):
-        if initial_segments is None:
-            initial_segments = deque([[0, 5], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5]])
-        self.segments = initial_segments
-        self.direction = 'RIGHT'
-        self.last_direction = self.direction
-        self.food = food
-        # w kolejności wskazówek zegara ↑→↓←
-        if key_mapping is None:
-            key_mapping = {
-                K_UP: 'UP',
-                K_RIGHT: 'RIGHT',
-                K_DOWN: 'DOWN',
-                K_LEFT: 'LEFT',
-            }
-        self.keys_to_directions = key_mapping
-        self.color = color
-
-    def _is_opposite(self, test_direction, direction):
-        opposite = self.opposites[direction]
-        if test_direction == opposite:
-            return True
-        return False
-
-    def _normalize_segments(self):
-        for segment in self.segments:
-            if segment[0] >= GAME_CELLS_X:
-                segment[0] -= GAME_CELLS_X
-            if segment[0] < 0:
-                segment[0] += GAME_CELLS_X
-            if segment[1] >= GAME_CELLS_Y:
-                segment[1] -= GAME_CELLS_Y
-            if segment[1] < 0:
-                segment[1] += GAME_CELLS_Y
-
-    def move(self):
-        vector = self.vectors.get(self.direction, (0, 0))
-        self.last_direction = self.direction
-        # wypadałoby zalogować, że brakuje jakiegos klucza...
-        first_segment = self.segments[-1]
-        self.segments.append(
-            # TODO: a może da się sprytniej? Coś z zip?
-            [first_segment[0] + vector[0], first_segment[1] + vector[1]]
-        )
-        self._normalize_segments()
-        if not self.try_to_eat():
-            self.segments.popleft()
-
-    def draw(self, surface):
-        for segment in self.segments:
-            draw_segment(surface, self.color, *segment)
-
-    def process_event_2(self, event):
-        # te stałe stringi wypadałoby do jakiś constów przenieść
-        if event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                if not self.last_direction == 'RIGHT':
-                    self.direction = 'LEFT'
-            elif event.key == K_RIGHT:
-                if not self.last_direction == 'LEFT':
-                    self.direction = 'RIGHT'
-            elif event.key == K_UP:
-                if not self.last_direction == 'DOWN':
-                    self.direction = 'UP'
-            elif event.key == K_DOWN:
-                if not self.last_direction == 'UP':
-                    self.direction = 'DOWN'
-
-    def process_event(self, event):
-        if event.type == KEYDOWN:
-            desired_direction = self.keys_to_directions.get(event.key)
-            if desired_direction:
-                if not self._is_opposite(desired_direction, self.last_direction):
-                    self.direction = desired_direction
-
-    def try_to_eat(self):
-        if (
-            self.segments[-1][0] == self.food.x
-            and self.segments[-1][1] == self.food.y
-        ):
-            self.food.eaten()
-            return True
-        return False
-
-
-class FoodProvider:
-    def __init__(self):
-        # A tu jest subtelny bug - możemy postawić jedzenie na wężu!!!!!
-        self._get_new_coords()
-
-    def _get_new_coords(self):
-        self.x = random.randrange(GAME_CELLS_X)
-        self.y = random.randrange(GAME_CELLS_Y)
-
-    def draw(self, surface):
-        draw_food(surface, self.x, self.y)
-
-    def eaten(self):
-        self._get_new_coords()
-
 
 def draw(surface, board):
     GROUND_COLOR = (0, 0, 50)
@@ -213,97 +59,69 @@ def draw(surface, board):
     BUTTON_FLAG = (0,50,150)
     ground = (
         0, 0,
-        640, 360
+        __WINDOW_SIZE__[0],__WINDOW_SIZE__[1]
     )
-    pygame.draw.rect(surface, GROUND_COLOR, ground)
-    XSize=len(board[0])
-    YSize=len(board)  
-    border=10
-    size=((ground[2]-5)/YSize,(ground[3]-5)/XSize)
-    for j in range(0,XSize):
-        for i in range(0,YSize):                     
+    pygame.draw.rect(surface, GROUND_COLOR, ground)    
+    for j in range(0,__AMOUNT_OF_BOMBS__[1]):
+        for i in range(0,__AMOUNT_OF_BOMBS__[0]):                     
             position = (
-                2+border/2+size[0]*i, 2+border/2+size[1]*j,
-                size[0]-border/2, size[1]-border/2
+                __PERIMETER__[0]+__BORDER__[0]+__BUTTON_SIZE__[0]*i,__PERIMETER__[1]+__BORDER__[1]+__BUTTON_SIZE__[1]*j, 
+                __BUTTON_SIZE__[0]-__BORDER__[0],__BUTTON_SIZE__[1]-__BORDER__[1]
             )
             BUTTON_COLOR=BUTTON_COV
-            if board[i][j].flag==False :
-                BUTTON_COLOR=BUTTON_COLOR
-            else:
+            if board[i][j].flag==True :
                 BUTTON_COLOR=BUTTON_FLAG
-
-            if board[i][j].uncovered==False :
-                BUTTON_COLOR=BUTTON_COLOR
-            else:
+            if board[i][j].uncovered==True:
                 BUTTON_COLOR=BUTTON_UNCOV
-
             pygame.draw.rect(surface, BUTTON_COLOR, position)
+    
+def position_to_button(pos):
+    button_pos=((pos[0]-__PERIMETER__[0])/__BUTTON_SIZE__[0],(pos[1]-__PERIMETER__[1])/__BUTTON_SIZE__[1])
+    t=False
+    #if |floor(button(x))-center_of_buttron(x))| <= (center_of_buttron(x)-border(x)) and |floor(button(y))-center_of_buttron(y))| <= (center_of_buttron(y)-border(y)):
+    if abs((int(mantissa(button_pos[0])*__BUTTON_SIZE__[0])%__BUTTON_SIZE__[0])-__BUTTON_SIZE__[0]/2) <= ((__BUTTON_SIZE__[0]/2)-__BORDER__[0]):
+        if abs((int(mantissa(button_pos[1])*__BUTTON_SIZE__[1])%__BUTTON_SIZE__[1])-__BUTTON_SIZE__[1]/2) <= ((__BUTTON_SIZE__[1]/2)-__BORDER__[1]):
+            t=True
+    button_pos=(ceil(button_pos[0]),ceil(button_pos[1]))
+    return (t,button_pos[0]-1,button_pos[1]-1)
             
-
-
-
-
+def MouseClick(input, board):
+    t,y,x = input
+    if t and board[y][x].uncovered==False:
+        print("uncover button ",y,x)
+        board[y][x].uncovered=True
 
 def run_game():
     pygame.init()
-    # workaround for: https://github.com/pygame/pygame/issues/331
     pygame.mixer.quit()
     fpsClock = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode(
-        (640,360)#(GAME_CELLS_X * GAME_CELL_SIZE_PX, GAME_CELLS_Y * GAME_CELL_SIZE_PX)        
+        __WINDOW_SIZE__        
     )
     pygame.display.set_caption('Minesweaper')
-    board= [9*[Sqr]]*16
-    random_bombs(board)
-    #food = FoodProvider()
-    #snakes = []
-    #snakes.append(Snake(food=food))
-    #snakes.append(
-    #    Snake(
-    #        food=food,
-    #        color=(0, 255, 0),
-    #        key_mapping={
-    #            K_w: 'UP',
-    #           K_d: 'RIGHT',
-    #            K_s: 'DOWN',
-    #            K_a: 'LEFT',
-    #        },
-    #        initial_segments=deque([[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3]]),
-    #    )
-    #)
-    #snakes.append(
-    #    Snake(
-    #        food=food,
-    #       color=(0, 0, 255),
-    #       key_mapping={
-    #           K_i: 'UP',
-    #           K_l: 'RIGHT',
-    #            K_k: 'DOWN',
-    #            K_j: 'LEFT',
-    #       },
-    #        initial_segments=deque([[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1]]),
-    #    )
-    #)
-    #frames_elapsed_since_last_move = 0
+    board = []
+    for i in range(__AMOUNT_OF_BOMBS__[0]):
+        row = []
+        for i in range(__AMOUNT_OF_BOMBS__[1]):
+            row.append(Sqr())
+        board.append(row)
+    pos_a=0
     while True:
         for event in pygame.event.get():
             print('event: {}'.format(event))
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-    #        for snake in snakes:
-    #            snake.process_event(event)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos_b = position_to_button(pygame.mouse.get_pos())
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos_a = position_to_button(pygame.mouse.get_pos())
+                if pos_a==pos_b:
+                    MouseClick(pos_a,board)
+
         draw(DISPLAYSURF,board)
-    #    for snake in snakes:
-    #        snake.draw(DISPLAYSURF)
-    #    food.draw(DISPLAYSURF)
-    #    frames_elapsed_since_last_move += 1
-    #    if frames_elapsed_since_last_move >= FRAMES_PER_MOVE:
-    #        frames_elapsed_since_last_move = 0
-    #        for snake in snakes:
-    #            snake.move()
         pygame.display.update()
-        fpsClock.tick(FPS)
+        fpsClock.tick(__FPS__)
 
 if __name__ == '__main__':
     run_game()
